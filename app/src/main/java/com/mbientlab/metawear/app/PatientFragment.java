9923337f5788011163b360cd7219d36ad334ca78
jsomer15 <janelle.somerville15@me.com>
2018-02-06 17:14:47 -0800

    Add more comments to the code and enter the delay values for each angle, as found in MATLAB

--- app/src/main/java/com/mbientlab/metawear/app/PatientFragment.java
+++ app/src/main/java/com/mbientlab/metawear/app/PatientFragment.java
@@ -81,28 +81,77 @@ public class PatientFragment extends PatientFragmentBase {
     private int roll_flipped = 0; //set to 1 when the data is flipped
     private int yaw_flipped = 0; //set to 1 when the data is flipped
 
-    public float[] pitch_data = new float[capacity];
-    public float[] roll_data = new float[capacity];
-    public float[] yaw_data = new float[capacity];
+    public float[] pitch_data = new float[capacity]; //this will be used to store unfiltered data
+    public float[] roll_data = new float[capacity]; //this will be used to store unfiltered data
+    public float[] yaw_data = new float[capacity]; //this will be used to store unfiltered data
 
-    public float[] pitch_b = {};
-    public float[] roll_b = {};
-    public float[] yaw_b = {};
+    //this has the definitions for the b values for convolution, gathered from MATLAB's fir1
+    public double[] pitch_b = {0.000779956276928257, 0.000840665872371555, 0.000894497959742675,
+            0.000922928033512174, 0.000893085626721506, 0.00076116965958513, 0.000478545449042225,
+            -5.73837960208593E-19, -0.000706696809086334, -0.00165102681769932, -0.00281099159279643,
+            -0.00412665195241372, -0.00549703415158719, -0.00678121936932106, -0.00780404235062265,
+            -0.00836635873181521, -0.00825934535557941, -0.00728182559480732, -0.00525921393862945,
+            -0.00206239663729071, 0.00237525704322728, 0.00804450547898497, 0.0148519892234465,
+            0.0226182590142883, 0.0310826856028395, 0.0399152567398694, 0.0487346385904925,
+            0.0571312865009099, 0.0646938920482515, 0.0710371002022807, 0.0758282593670764,
+            0.0788109978766488, 0.0798236534708593, 0.0788109978766488, 0.0758282593670764,
+            0.0710371002022807, 0.0646938920482515, 0.0571312865009099, 0.0487346385904925,
+            0.0399152567398694, 0.0310826856028395, 0.0226182590142883, 0.0148519892234465,
+            0.00804450547898497, 0.00237525704322728, -0.00206239663729071, -0.00525921393862945,
+            -0.00728182559480732, -0.00825934535557941, -0.00836635873181521, -0.00780404235062265,
+            -0.00678121936932106, -0.00549703415158719, -0.00412665195241372, -0.00281099159279643,
+            -0.00165102681769932, -0.000706696809086334, -5.73837960208593E-19, 0.000478545449042225,
+            0.00076116965958513, 0.000893085626721506, 0.000922928033512174, 0.000894497959742675,
+            0.000840665872371555, 0.000779956276928257};
+    public double[] roll_b = {0.000779956276928257, 0.000840665872371555, 0.000894497959742675,
+            0.000922928033512174, 0.000893085626721506, 0.00076116965958513, 0.000478545449042225,
+            -5.73837960208593E-19, -0.000706696809086334, -0.00165102681769932, -0.00281099159279643,
+            -0.00412665195241372, -0.00549703415158719, -0.00678121936932106, -0.00780404235062265,
+            -0.00836635873181521, -0.00825934535557941, -0.00728182559480732, -0.00525921393862945,
+            -0.00206239663729071, 0.00237525704322728, 0.00804450547898497, 0.0148519892234465,
+            0.0226182590142883, 0.0310826856028395, 0.0399152567398694, 0.0487346385904925,
+            0.0571312865009099, 0.0646938920482515, 0.0710371002022807, 0.0758282593670764,
+            0.0788109978766488, 0.0798236534708593, 0.0788109978766488, 0.0758282593670764,
+            0.0710371002022807, 0.0646938920482515, 0.0571312865009099, 0.0487346385904925,
+            0.0399152567398694, 0.0310826856028395, 0.0226182590142883, 0.0148519892234465,
+            0.00804450547898497, 0.00237525704322728, -0.00206239663729071, -0.00525921393862945,
+            -0.00728182559480732, -0.00825934535557941, -0.00836635873181521, -0.00780404235062265,
+            -0.00678121936932106, -0.00549703415158719, -0.00412665195241372, -0.00281099159279643,
+            -0.00165102681769932, -0.000706696809086334, -5.73837960208593E-19, 0.000478545449042225,
+            0.00076116965958513, 0.000893085626721506, 0.000922928033512174, 0.000894497959742675,
+            0.000840665872371555, 0.000779956276928257};
+    public double[] yaw_b = {0.000779956276928257, 0.000840665872371555, 0.000894497959742675,
+            0.000922928033512174, 0.000893085626721506, 0.00076116965958513, 0.000478545449042225,
+            -5.73837960208593E-19, -0.000706696809086334, -0.00165102681769932, -0.00281099159279643,
+            -0.00412665195241372, -0.00549703415158719, -0.00678121936932106, -0.00780404235062265,
+            -0.00836635873181521, -0.00825934535557941, -0.00728182559480732, -0.00525921393862945,
+            -0.00206239663729071, 0.00237525704322728, 0.00804450547898497, 0.0148519892234465,
+            0.0226182590142883, 0.0310826856028395, 0.0399152567398694, 0.0487346385904925,
+            0.0571312865009099, 0.0646938920482515, 0.0710371002022807, 0.0758282593670764,
+            0.0788109978766488, 0.0798236534708593, 0.0788109978766488, 0.0758282593670764,
+            0.0710371002022807, 0.0646938920482515, 0.0571312865009099, 0.0487346385904925,
+            0.0399152567398694, 0.0310826856028395, 0.0226182590142883, 0.0148519892234465,
+            0.00804450547898497, 0.00237525704322728, -0.00206239663729071, -0.00525921393862945,
+            -0.00728182559480732, -0.00825934535557941, -0.00836635873181521, -0.00780404235062265,
+            -0.00678121936932106, -0.00549703415158719, -0.00412665195241372, -0.00281099159279643,
+            -0.00165102681769932, -0.000706696809086334, -5.73837960208593E-19, 0.000478545449042225,
+            0.00076116965958513, 0.000893085626721506, 0.000922928033512174, 0.000894497959742675,
+            0.000840665872371555, 0.000779956276928257};
 
     int sizeofpitchb = pitch_b.length;
     int sizeofpitchdata = pitch_data.length;
-    int numpitchrows = (sizeofpitchdata + sizeofpitchb) - 1;
-    float[] pitch_filtered = new float[numpitchrows];
+    int numpitchrows = (sizeofpitchdata + sizeofpitchb) - 1; //minus 1 since arrays start at 0
+    float[] pitch_filtered = new float[numpitchrows]; //y is longer than x by b-1 post convolution
 
     int sizeofrollb = roll_b.length;
     int sizeofrolldata = roll_data.length;
-    int numrollrows = (sizeofrolldata + sizeofrollb) - 1;
-    float[] roll_filtered = new float[numrollrows];
+    int numrollrows = (sizeofrolldata + sizeofrollb) - 1; //minus 1 since arrays start at 0
+    float[] roll_filtered = new float[numrollrows]; //y is longer than x by b-1 post convolution
 
     int sizeofyawb = yaw_b.length;
     int sizeofyawdata = yaw_data.length;
-    int numyawrows = (sizeofyawdata + sizeofyawb) - 1;
-    float[] yaw_filtered = new float[numyawrows];
+    int numyawrows = (sizeofyawdata + sizeofyawb) - 1; //minus 1 since arrays start at 0
+    float[] yaw_filtered = new float[numyawrows]; //y is longer than x by b-1 post convolution
 
     public PatientFragment() {
         super(R.string.navigation_fragment_patient, R.layout.fragment_patientdata, -1f, 1f);
@@ -142,6 +191,7 @@ public class PatientFragment extends PatientFragmentBase {
 
             //this is new code added by Janelle to save the data into a circular array
             //this for loop shift all the data along in the array before adding a new data point
+            //this clears slot 0 and eliminates the least recent data point
             for (int i = (capacity - 1); i > 0; i--) {
                 pitch_data[i] = pitch_data[i - 1];
                 roll_data[i] = roll_data[i - 1];
@@ -149,33 +199,29 @@ public class PatientFragment extends PatientFragmentBase {
             }
 
             //store each angle as the first entry in the array and flip data
-            pitch_data[0] = angles.pitch();
+            pitch_data[0] = angles.pitch(); //this should return the current pitch as a float
             pitch_flipped = FlipCheck(pitch_data, pitch_flipped);
-            if (pitch_flipped == 1) {
+            if (pitch_flipped == 1) { //if we get that the current value is flipped, we must add 360
                 pitch_data[0] = pitch_data[0] + 360;
             }
 
-            roll_data[0] = angles.roll();
+            roll_data[0] = angles.roll(); //this should return the current roll as a float
             roll_flipped = FlipCheck(roll_data, roll_flipped);
-            if (roll_flipped == 1) {
+            if (roll_flipped == 1) { //if we get that the current value is flipped, we must add 360
                 roll_data[0] = roll_data[0] + 360;
             }
 
-            yaw_data[0] = angles.yaw();
+            yaw_data[0] = angles.yaw(); //this should return the current yaw as a float
             yaw_flipped = FlipCheck(yaw_data, yaw_flipped);
-            if (yaw_flipped == 1) {
+            if (yaw_flipped == 1) { //if we get that the current value is flipped, we must add 360
                 yaw_data[0] = yaw_data[0] + 360;
             }
 
-            //call the Convolution function
+            //call the Convolution function, filtered data is separate as we want to fourier unfiltered
             pitch_filtered = Convolution(pitch_b, pitch_data);
             roll_filtered = Convolution(roll_b, roll_data);
             yaw_filtered = Convolution(yaw_b, yaw_data);
 
-            //this just adds the new point at the next slot in the array, not the first
-            /*index++;
-            if (index == capacity)
-                index = 0;*/
 
             //the new code ends here
 
@@ -242,8 +288,19 @@ public class PatientFragment extends PatientFragmentBase {
         adapter.add(new HelpOption(R.string.config_name_sensor_fusion_data, R.string.config_desc_sensor_fusion_data));
     }
 
-
-    private static float[] Convolution(float[] b, float[] data) {
+    //This function performs the convolution as a part of the FIR filter, first we must set our x
+    //to a 2D array, each row corresponds to 1 set of values that will be multiplied by b
+    //for example if we want [1 2 3 4 5 6] to be multiplied by [0.1 0.2 0.3] our 2D array would be
+    //[1 0 0]
+    //[2 1 0]
+    //[3 2 1]
+    //[4 3 2]
+    //[5 4 3]
+    //[6 5 4]
+    //[0 6 5]
+    //[0 0 6]
+    //this function returns our y post convolution of y = b*x
+    private static float[] Convolution(double[] b, float[] data) {
         int sizeofb = b.length; //the size of b
         int sizeofdata = data.length; //the number of data points we are storing
         int numrows = (sizeofdata + sizeofb) - 1; //the number of rows depends on the number of delays
@@ -270,11 +327,12 @@ public class PatientFragment extends PatientFragmentBase {
         for (r = 0; r < numrows; r++) { //multiply each row by b and sum
             float sum = 0;
             for (c = 0; c < sizeofb; c++) {
-                sum = (multi[r][c] * b[c]) + sum;
+                sum = (multi[r][c] * (float)b[c]) + sum;
             }
             y[r] = sum;
         }
 
+        //The code below was used to print out the results of convolution and was used for debugging
         /*for(r = 0; r < 7; r++){
             for(c = 0; c < 3; c++){
                 System.out.println(multi[r][c]);
@@ -291,25 +349,33 @@ public class PatientFragment extends PatientFragmentBase {
 
     }
 
+    //this function is used to resolve the fact that the raw Euler Angles from the sensor do are
+    //being flipped when they are over 360 degrees, for example 361 degrees would be sent as 1 degree
     public static int FlipCheck(float[] data, int alreadyflipped) {
-        int i = 0;
-        int flip;
+        int i = 0; //this is used as an index to get the previous data point and current
+        int flip; //this returned, 0 is false, 1 is true
         float lastval = data[i + 1];
         float currval = data[i];
 
-        if (alreadyflipped == 1) {
+        //if the data is already flipped we are checking for the point when the current angle is
+        //less than 360 and the last was over 360
+        if (alreadyflipped == 1) {  //alreadyflipped is passed in
             if ((lastval > 360) && (currval > 300))
-                flip = 0;
+                flip = 0; //if it is no longer flipped, we return false
             else
-                flip = 1;
-        } else {
+                flip = 1; //otherwise we return true
+        }
+
+        //if the last point was not flipped then we are checking if the last point was over 300
+        //and the current is under 50--> I can show all this logic graphically if it's confusing
+        else {
             if ((lastval > 300) && (currval < 50)) {
                 flip = 1;
             } else
                 flip = 0;
         }
 
-        return flip;
+        return flip; //basically returning a true or false on whether the data is still flipped
     }
 
 }
